c05e6d583534e511198d91b883cd2c99af00e03d
Andrei Maximov <andrei.maximov@gmail.com>
2018-05-22 23:12:12 -0700

    Fix destruction of last thread

--- src/worker.cpp
+++ src/worker.cpp
@@ -33,6 +33,8 @@ void Worker::loop() {
     queue_.pop();
     this_thread_->run();
   }
+
+  this_thread_.reset();
 }
 
 }
--- tests/worker.cpp
+++ tests/worker.cpp
@@ -4,6 +4,20 @@
 
 namespace uthread {
 
+static constexpr int DESTROY = 0;
+static constexpr int FOO = 1;
+static int ops[3] = {0, 0, 0};
+
+struct Thingy {
+  ~Thingy() {
+    ops[DESTROY]++;
+  }
+
+  void foo() {
+    ops[FOO]++;
+  }
+};
+
 TEST(WorkerTest, RunSingleThread) {
   int x = 0;
   Worker worker;
@@ -67,6 +81,36 @@ TEST(WorkerTest, SwitchToNextOnFinish) {
   worker.loop();
 
   ASSERT_EQ(x, 2);
+}
+
+TEST(WorkerTest, ThreadLifetime) {
+  Thingy x;
+  Worker worker;
+
+  ASSERT_EQ(ops[DESTROY], 0);
+  ASSERT_EQ(ops[FOO], 0);
+
+  auto f = Thread::create([=]() mutable {
+    x.foo();
+  });
+
+  auto g = Thread::create([=]() mutable {
+    x.foo();
+  });
+
+  // https://lonelycoding.com/move-semantic-with-stdfunction-2/
+  //
+  // tldr; We can't guarantee how many times x has been copied and destroyed
+  // in the thread lambda.
+  auto destroyed = ops[DESTROY];
+
+  worker.schedule(std::move(f));
+  worker.schedule(std::move(g));
+  worker.loop();
+
+  // Threads have ran, make sure closure, etc. have been destroyed.
+  ASSERT_EQ(ops[DESTROY], destroyed + 2);
+  ASSERT_EQ(ops[FOO], 2);
 }
 
 }
